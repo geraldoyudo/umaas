376654c2a37b2faa3fa500b6b68ef6eb0950e450
Gerald Oyudo <gerald.oyudo@isslng.com>
2016-09-26 19:06:58 +0100

    adapted project to war

--- code/umaas/umaas-file/src/main/java/com/gerald/umaas/file/UmaasFileApplication.java
+++ code/umaas/umaas-file/src/main/java/com/gerald/umaas/file/UmaasFileApplication.java
@@ -2,10 +2,15 @@ package com.gerald.umaas.file;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UmaasFileApplication {
-
+public class UmaasFileApplication extends SpringBootServletInitializer{
+  @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UmaasFileApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(UmaasFileApplication.class, args);
 	}
