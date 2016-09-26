9282c51f016c7921a27824d8a1470a8cace06608
Gerald Oyudo <gerald.oyudo@isslng.com>
2016-09-26 17:21:29 +0100

    updated proxy configuration

--- code/umaas/umaas-domain-admin/src/main/java/com/gerald/umaas/domain_admin/config/ProxyConfiguration.java
+++ code/umaas/umaas-domain-admin/src/main/java/com/gerald/umaas/domain_admin/config/ProxyConfiguration.java
@@ -10,7 +10,8 @@ import org.springframework.stereotype.Component;
 public class ProxyConfiguration extends RouteBuilder {
 	@Value("${umaas.core}")
 	private String coreUrl;
-	
+	@Value("${umaas.domainAdmin.contextPath:}")
+	private String contextPath;
 	@PostConstruct
 	public void init(){
 		coreUrl = coreUrl.replace("http", "http4")
@@ -20,9 +21,12 @@ public class ProxyConfiguration extends RouteBuilder {
 	}
 	@Override
 	public void configure() throws Exception {
+		if(contextPath == null)
+			contextPath = "";
+		String forwardPrefix = contextPath + "/umaas/core";
 		 from("servlet://core?matchOnUriPrefix=true")
 		.setHeader("X-Forwarded-Host", simple("${headers.host}"))
-		.setHeader("X-Forwarded-Prefix", simple("/umaas/core"))
+		.setHeader("X-Forwarded-Prefix", simple(forwardPrefix))
 		 .to(coreUrl + "?bridgeEndpoint=true&throwExceptionOnFailure=false");
 		 
 		 from("servlet:///test?httpMethodRestrict=GET")
